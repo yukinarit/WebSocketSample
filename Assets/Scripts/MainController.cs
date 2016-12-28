3416fdac0817f93e4ccfbe7f454f1cf4caf8dac0
Yukinari Tani <yukinarit84@gmail.com>
2016-12-28 17:38:29 +0900

    Client connects to WebSocket server

--- Assets/Scripts/MainController.cs
+++ Assets/Scripts/MainController.cs
@@ -1,16 +1,56 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
+
+using WebSocketSharp;
+
+namespace WebSocketSample
+{
 
 public class MainController : MonoBehaviour
 {
-	// Use this for initialization
+	WebSocket ws; // WebSocketコネクション
+
 	void Start()
 	{
+		// WebSocketのコネクション作成
+		ws = new WebSocket("ws://localhost:5678");
+
+		//
+		// ハンドラの設定
+		//
+
+		// コネクション確立したときのハンドラ
+		ws.OnOpen += (sender, e) =>
+		{
+			Debug.Log("WebSocket opened.");
+		};
+
+		// メッセージを受信したときのハンドラ
+		ws.OnMessage += (sender, e) =>
+		{
+			Debug.Log("WebSocket Message: " + e.Data);
+		};
+
+		// エラーが発生したときのハンドラ
+		ws.OnError += (sender, e) =>
+		{
+			Debug.Log("WebSocket Error Message: " + e.Message);
+		};
+
+		// コネクションを閉じたときのハンドラ
+		ws.OnClose += (sender, e) =>
+		{
+			Debug.Log("WebSocket Close");
+		};
+
+		// サーバーへ接続
+		ws.Connect();
 	}
 
-	// Update is called once per frame
 	void Update()
 	{
-
 	}
 }
+
+} // namespace WebSocketSample
